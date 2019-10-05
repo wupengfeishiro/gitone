fd4469864d83bb97a689116b3f5698046566a222
wupengfeishiro <1930369489@qq.com>
2019-10-05 19:43:10 +0800

    提交项目

---
+++ src/main/java/com/qf/gitone/GitoneApplication.java
@@ -0,0 +1,13 @@
+package com.qf.gitone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitoneApplication.class, args);
+	}
+
+}
